8fa72b358074b970e133b163259b20f6b1582571
beccaris <andreabeccaris88@gmail.com>
2016-10-27 20:32:57 +0200

    add a first api layer

---
+++ main.py
@@ -0,0 +1,34 @@
+from flask import Flask, jsonify, abort
+app = Flask(__name__)
+
+sessions = [
+    {
+        'id': 1,
+        'title': u'Buy groceries',
+        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': u'Learn Python',
+        'description': u'Need to find a good Python tutorial on the web',
+        'done': False
+    }
+]
+
+@app.route("/api/session/all", methods=['GET'])
+def get_all_session():
+    return jsonify({"sessions":sessions})
+
+@app.route("/api/session/<int:session_id>", methods=['GET'])
+def get_single_session(session_id):
+    session = [session for session in sessions if session['id']==session_id]
+    if len(session)>0:
+        return jsonify({'session':session})
+    else:
+        abort(404)
+
+
+
+if __name__ == "__main__":
+    app.run()
