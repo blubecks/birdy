37e9d85f5134836a0f2ef4938e9dd2cf0d31e2d7
root <root@template.(none)>
2016-10-27 22:49:49 +0200

    api call works, now i need a parser

--- BIRD/bird.py
+++ BIRD/bird.py
@@ -1,5 +1,9 @@
 import subprocess
 
 def greetings(name):
-    output = subprocess.Popen(["/usr/bin/birdc", "show protocols all"], stdout=subprocess.PIPE).communicate()[0]
-    print output
+    output = subprocess.Popen(["/usr/local/sbin/birdc", "show protocols all"], stdout=subprocess.PIPE).communicate()[0]
+    for line in output.split():
+        print "#########"
+        print line
+        print "#########"
+    return output
