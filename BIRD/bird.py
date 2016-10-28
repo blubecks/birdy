50325ccdcf68dbbec7bf355790ea8e64889dce24
root <root@template.(none)>
2016-10-28 12:13:06 +0200

    fix

--- BIRD/bird.py
+++ BIRD/bird.py
@@ -2,7 +2,7 @@ import subprocess, re
 def all_bgp_session():
     sessions = []
     output = subprocess.Popen(["/usr/local/sbin/birdc", "show protocols all"], stdout=subprocess.PIPE).communicate()[0]
-    for line in output:
+    for line in output.split('\n'):
         object_parsed = re.match( r'^(\w+)\s+BGP\s+(\w+)\s+(\w+)\s+([0-9\-\:]+)\s+(\w+).*$', line, re.M|re.I)
         if object_parsed:
             s = {}
