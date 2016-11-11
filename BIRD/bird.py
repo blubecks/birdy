3b6af83f5bf77e00019276e67fa4c6a7d1e020d3
beccaris <andreabeccaris88@gmail.com>
2016-11-11 15:16:13 +0100

    working on auto config

--- BIRD/bird.py
+++ BIRD/bird.py
@@ -1,14 +1,70 @@
 import subprocess, os
 from Parser.parser import Parser
-class Bird:
+from validator import Required, Pattern, validate, Length
+
+
+class Bird(object):
     def __init__(self, app):
-        self.app = app
+        self.app = {
+            'bird_instance': app.config['BIRDC_INSTANCE'],
+            'bird_v6_instance': app.config['BIRDC6_INSTANCE'],
+            'secret_key': app.config['SECRET_KEY'],
+            'debug': app.config['DEBUG']
+        }
 
     def all_bgp_session(self):
-        if self.app.config['DEBUG']:
+        if self.app['debug']:
             fake_data = os.path.join(os.path.dirname(__file__), 'summary.txt')
             f = open(fake_data, 'r')
             output = f.read()
         else:
-            output = subprocess.Popen([self.app.config['BIRDC_INSTANCE'], "show protocols all"], stdout=subprocess.PIPE).communicate()[0]
+            output = subprocess.Popen([self.app['bird_instance'], "show protocols all"], stdout=subprocess.PIPE).communicate()[0]
         return Parser.parse_output_to_sessions(output)
+
+    def configure_new_session(self, data = None):
+        if not data:
+            return False
+        try:
+            self._check_params(data)
+        except BirdException as b:
+            return {
+                'status': 'error',
+                'message': b.message
+            }
+        # Write configuration on file
+        return {
+            'status': 'success',
+            'code': 200,
+            'message': 'configuration ok!'
+        }
+
+    def _check_params(self, data):
+        rules = {
+            "as": [Required, Length(1, maximum=15)],
+            "name": [Required, Length(1, maximum=15)],
+            "ip": [Required, Length(1, maximum=15)],
+            "ixtype": [Required, Length(1, maximum=15)],
+            "asexport": [Required, Length(1, maximum=15)]
+        }
+        if 'secret' not in data or data['secret'] != self.app['secret_key']:
+            raise BirdException('you are unauthorized!')
+
+        result = validate(rules, data)
+        if result[0]:
+            return True
+        else:
+            raise BirdException(result[1])
+
+
+class BirdException(Exception):
+    pass
+
+'''
+as
+name
+ip
+[11/11/2016, 10:52:24] Enrico Pellegrini: ixtype
+[11/11/2016, 10:52:27] Enrico Pellegrini: asexport
+[11/11/2016, 10:52:30] Enrico Pellegrini: maxpref
+[11/11/2016, 10:52:36] Enrico Pellegrini: custom_commands
+'''
--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, abort, request
 from BIRD.bird import Bird
+import json
 
 
 app = Flask(__name__)
@@ -9,18 +10,20 @@ bird = Bird(app)
 
 @app.route("/api/session/all", methods=['GET'])
 def get_all_session():
-    return jsonify({"sessions":bird.all_bgp_session()})
+    return jsonify({"sessions": bird.all_bgp_session()})
+
 
 @app.route("/api/session/<int:session_id>", methods=['GET'])
 def get_single_session(session_id):
     abort(404)
 
+
 @app.route("/api/configure/session", methods=['POST'])
 def add_config():
-    print request.args
-    return jsonify({"ok":"ok"})
-
+    data = request.data
+    data_dict = json.loads(data)
+    return jsonify(bird.configure_new_session(data_dict))
 
 
 if __name__ == "__main__":
-    app.run(host= '0.0.0.0')
+    app.run(host='0.0.0.0')
